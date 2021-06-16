f4dccbbca8553a3a0ad6cf4f3de880bb8eccea50
n-dub <50988318+n-dub@users.noreply.github.com>
2021-06-16 21:20:48 +0500

    job handle is now used with a unique ptr

---
+++ FerrumAssetCompiler/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <array>
+#include <FerrumCore/FeLog.h>
+#include <FerrumCore/FeVector.h>
+#include <FerrumCore/FeJobSystem.h>
+
+using namespace Ferrum;
+
+int main() {
+	FeLogInit();
+}
