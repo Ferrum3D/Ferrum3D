45c54bb179525da48b5b4e35f003d45db1a46bcb
n-dub <50988318+n-dub@users.noreply.github.com>
2021-08-01 17:58:05 +0500

    Custom shared pointer Implemented reference counting pointer to use with custom allocators

--- FeCore/FeCore/Memory/Memory.h
+++ FeCore/FeCore/Memory/Memory.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <Memory/Allocator.h>
 #include <Memory/HeapAllocator.h>
-#include <Modules/Singleton.h>
+#include <Memory/SharedPtr.h>
 #include <memory>
 
 namespace FE
@@ -22,5 +22,17 @@ namespace FE
         void* data = FE::GlobalAllocator<FE::HeapAllocator>::Get().Allocate(sizeof(T), alignof(T), position);
         T* obj     = new (data) T(std::forward<Args>(args)...);
         return std::unique_ptr<T, TObjectDeleter<T, FE::HeapAllocator>>(obj, ObjectDeleter<T, FE::HeapAllocator>);
+    }
+
+    template<class T, class... Args>
+    auto MakeShared(Args&&... args) -> RefCountPtr<T>
+    {
+        FE_STATIC_SRCPOS(position);
+        using TObject = Internal::RefCountObject<T>;
+        IAllocator* allocator = &FE::GlobalAllocator<FE::HeapAllocator>::Get();
+        void* data = allocator->Allocate(sizeof(TObject), alignof(TObject), position);
+        auto* obj = new (data) TObject(allocator);
+        obj->Construct(std::forward<Args>(args)...);
+        return RefCountPtr<T>(obj);
     }
 } // namespace FE
--- FeTestProject/main.cpp
+++ FeTestProject/main.cpp
@@ -1,11 +1,30 @@
 #include <FeCore/Console/FeLog.h>
 #include <FeCore/Memory/Memory.h>
 
+class LargeClass
+{
+public:
+    LargeClass()
+    {
+        FE_LOG_MESSAGE("Allocating LargeClass {}", sizeof(*this));
+        for (char& c : Data)
+            c = 0;
+    }
+
+    ~LargeClass()
+    {
+        FE_LOG_MESSAGE("Deallocating LargeClass");
+    }
+
+    char Data[256 * 1024 * 1024];
+};
+
 int main()
 {
     FE::Env::CreateEnvironment();
     FE::GlobalAllocator<FE::HeapAllocator>::Init(FE::HeapAllocatorDesc{});
-    auto logger = FE::MakeUnique<FE::Debug::ConsoleLogger>();
+    auto logger    = FE::MakeShared<FE::Debug::ConsoleLogger>();
+    auto large     = FE::MakeShared<LargeClass>();
     FE::String str = "loooooooooooooooooooooooooong";
     for (int i = 0; i < 4; ++i)
     {
