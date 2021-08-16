dd9fc29e469e2b979f89fd8c6a6e8451cda7973b
n-dub <ragucoz@gmail.com>
2021-08-16 17:54:12 +0500

    Added entt submodule

--- cmake/ThirdParty/entt.cmake
+++ cmake/ThirdParty/entt.cmake
@@ -1,2 +1,2 @@
 add_library(entt INTERFACE)
-target_include_directories(entt INTERFACE "${FE_PROJECT_ROOT}/ThirdParty/entt")
+target_include_directories(entt INTERFACE "${FE_PROJECT_ROOT}/ThirdParty/entt/single_include")
