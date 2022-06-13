f02866ccb9276e05cf5c03fb5138b711a31f30a8
n-dub <50988318+n-dub@users.noreply.github.com>
2022-06-13 11:45:32 +0500

    Add cpp file pragma once validation

--- scripts/commit_validation/validate_headers.py
+++ scripts/commit_validation/validate_headers.py
@@ -1,20 +1,29 @@
 from scripts.common.ferrum_files import get_ferrum_files
 
-filenames = get_ferrum_files(extensions=["h"])
+filenames = get_ferrum_files(extensions=["h", "cpp"])
 error_count = 0
 
 
 def process_files():
     global error_count
     for filename in filenames:
-        with open(filename) as f:
-            if "#pragma once" not in f.readline():
-                print('=' * 80)
-                print("Validation error in file {}: No \"#pragma once\" in header".format(filename))
-                error_count += 1
+        try:
+            with open(filename) as f:
+                if "#pragma once" not in f.readline():
+                    if filename.endswith(".h"):
+                        print('=' * 80)
+                        print(f"Validation error in file {filename}: No \"#pragma once\" in header")
+                        error_count += 1
+                else:
+                    if filename.endswith(".cpp"):
+                        print('=' * 80)
+                        print(f"Validation error in file {filename}: \"#pragma once\" in cpp file")
+                        error_count += 1
+        except UnicodeDecodeError:
+            print(f'Cannot decode file {filename}')
 
     print("Checked {} files.".format(len(filenames)))
     if error_count == 0:
         print("Header validation finished without errors.")
     else:
-        print("Header validation finished, {} errors found.".format(error_count))
+        print("Header validation finished, {error_count} errors found.")
