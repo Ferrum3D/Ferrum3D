cb1cfd3b0480cae5962c3e5119ade595b55c708c
n-dub <ragucoz@gmail.com>
2022-06-13 11:44:33 +0500

    Fix decoding error in validate_type_id.py

--- scripts/commit_validation/validate_type_id.py
+++ scripts/commit_validation/validate_type_id.py
@@ -30,11 +30,11 @@ def proc_line(line_text, line_number, file_name, uuid_str):
         prev_idx = declarations.index(decl)
         prev = declarations[prev_idx]
         print('=' * 80)
-        print("Validation error at line {} of file {}: "
-              + "the specified UUID was declared twice".format(line_number, file_name))
-        print("Note: First declaration was at line {} of file {}".format(prev.line_number, prev.filename))
-        print("Note: The UUID was " + uuid_str)
-        print("Note: See declaration: {}".format(line_text))
+        print(f"Validation error at line {line_number} of file {file_name}: "
+              + "the specified UUID was declared twice")
+        print(f"Note: First declaration was at line {prev.line_number} of file {prev.filename}")
+        print(f"Note: The UUID was {uuid_str}")
+        print(f"Note: See declaration: {line_text}")
         error_count += 1
     except ValueError:
         pass
@@ -45,13 +45,16 @@ def proc_line(line_text, line_number, file_name, uuid_str):
 def process_files():
     for filename in filenames:
         with open(filename) as f:
-            for n, line in enumerate(f):
-                search_result = pattern.search(line)
-                if search_result:
-                    proc_line(line, n, filename, search_result.group(2))
+            try:
+                for n, line in enumerate(f):
+                    search_result = pattern.search(line)
+                    if search_result:
+                        proc_line(line, n, filename, search_result.group(2))
+            except UnicodeDecodeError:
+                print(f'Cannot decode file {filename}')
 
-    print("Checked {} files.".format(len(filenames)))
+    print(f"Checked {len(filenames)} files.")
     if error_count == 0:
         print("Type UUID validation finished without errors.")
     else:
-        print("Type UUID validation finished, {} errors found.".format(error_count))
+        print(f"Type UUID validation finished, {error_count} errors found.")
