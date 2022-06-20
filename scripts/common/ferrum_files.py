c3c9d34a831677c7981cb32c745dd51224281527
n-dub <ragucoz@gmail.com>
2022-06-20 09:14:01 +0500

    Filter files by filename in ferrum_files.py

--- scripts/common/ferrum_files.py
+++ scripts/common/ferrum_files.py
@@ -3,7 +3,7 @@ import os
 from functools import reduce
 
 
-def get_ferrum_files(extensions=None, engine_dirs=None):
+def get_ferrum_files(extensions=None, engine_dirs=None, filename=None):
     if engine_dirs is None:
         engine_dirs = ["FeAssetCompiler", "FeCore", "Modules", "FeTestProject", "Samples", "cmake"]
     if extensions is None:
@@ -11,4 +11,8 @@ def get_ferrum_files(extensions=None, engine_dirs=None):
 
     filenames = [glob.glob(".." + os.sep + d + '/**/*.' + e, recursive=True) for d in engine_dirs for e in extensions]
     filenames = reduce(list.__add__, filenames)
+
+    if filename is not None:
+        filenames = [f for f in filenames if filename in f]
+
     return filenames
