bee82f9ae56d6e1c02721f90180f08ea35501a19
n-dub <ragucoz@gmail.com>
2022-06-20 18:57:59 +0500

    Rewritten setup scripts

--- scripts/common/common.py
+++ scripts/common/common.py
@@ -6,12 +6,17 @@ def get_vs_path():
     return r'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community'
 
 
-def run_command(arguments, *, return_stdout=False):
-    proc = subprocess.run(arguments, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+def run_command(arguments, *, return_stdout=False, hide_output=True, working_directory=None):
+    stderr = subprocess.PIPE
+    if hide_output:
+        stdout = subprocess.PIPE
+    else:
+        stdout = None
+    proc = subprocess.run(arguments, stderr=stderr, stdout=stdout, cwd=working_directory)
     if proc.returncode:
         print(f'Process "{arguments[0]}" exited with code {proc.returncode}')
         print(f'Stderr:\n{proc.stderr.decode("utf-8")}')
     if not return_stdout:
         return proc.returncode == 0
     else:
-        return proc.stdout.decode('utf-8')
+        return proc.stdout.decode('utf-8') if proc.returncode == 0 else None
