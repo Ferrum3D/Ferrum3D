70d60ad3ebe093296788d7d7d1722fa3280a6025
n-dub <ragucoz@gmail.com>
2022-06-20 07:47:17 +0500

    Fix directory list ferrum_files.py

--- scripts/common/ferrum_files.py
+++ scripts/common/ferrum_files.py
@@ -5,7 +5,7 @@ from functools import reduce
 
 def get_ferrum_files(extensions=None, engine_dirs=None):
     if engine_dirs is None:
-        engine_dirs = ["FeAssetCompiler", "FeCore", "FeModules", "FeTestProject"]
+        engine_dirs = ["FeAssetCompiler", "FeCore", "Modules", "FeTestProject", "Samples", "cmake"]
     if extensions is None:
         extensions = ["cpp", "h"]
 
