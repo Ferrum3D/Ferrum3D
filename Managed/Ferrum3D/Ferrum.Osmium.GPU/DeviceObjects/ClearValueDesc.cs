645151c852c6549ed1b398c5c4bd23f11e77aeb7
n-dub <50988318+n-dub@users.noreply.github.com>
2022-06-21 13:45:55 +0500

    Moved BufferCopyRegion and ClearValueDesc to separate files

---
+++ Managed/Ferrum3D/Ferrum.Osmium.GPU/DeviceObjects/ClearValueDesc.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using Ferrum.Core.Math;
+
+namespace Ferrum.Osmium.GPU.DeviceObjects
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ClearValueDesc
+    {
+        public readonly Color Color;
+
+        public ClearValueDesc(Color color)
+        {
+            Color = color;
+        }
+    }
+}
