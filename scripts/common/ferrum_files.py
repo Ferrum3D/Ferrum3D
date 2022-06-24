062c003beb2cb587f26064c280753336a2ffbada
n-dub <50988318+n-dub@users.noreply.github.com>
2022-06-24 19:56:38 +0500

    Rename directories in ferrum_files.py

--- scripts/common/ferrum_files.py
+++ scripts/common/ferrum_files.py
@@ -5,7 +5,14 @@ from functools import reduce
 
 def get_ferrum_files(extensions=None, engine_dirs=None, filename=None):
     if engine_dirs is None:
-        engine_dirs = ["FeAssetCompiler", "FeCore", "Modules", "FeTestProject", "Samples", "cmake"]
+        engine_dirs = [
+            "FerrumAssetCompiler",
+            "FerrumCore",
+            "FerrumModules",
+            "FerrumTestProject",
+            "Samples",
+            "cmake"
+        ]
     if extensions is None:
         extensions = ["cpp", "h"]
 
