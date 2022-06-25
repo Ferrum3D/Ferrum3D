55047a473a1e6f45c236e74b375d100b0d5f8154
n-dub <ragucoz@gmail.com>
2022-06-25 10:18:22 +0500

    Reorder ImageBindFlags

--- FerrumModules/OsmiumGPU/OsGPU/Image/ImageEnums.h
+++ FerrumModules/OsmiumGPU/OsGPU/Image/ImageEnums.h
@@ -6,19 +6,17 @@ namespace FE::Osmium
 {
     enum class ImageBindFlags
     {
-        None            = 0,
+        None = 0,
+
         ShaderRead      = 1 << 0,
-        ShaderWrite     = 1 << 1,
-        ShaderReadWrite = ShaderRead | ShaderWrite,
+        UnorderedAccess = 1 << 7,
 
         RenderTarget = 1 << 2,
         Depth        = 1 << 3,
         Stencil      = 1 << 4,
 
         TransferRead  = 1 << 5,
-        TransferWrite = 1 << 6,
-
-        UnorderedAccess = 1 << 7
+        TransferWrite = 1 << 6
     };
 
     FE_ENUM_OPERATORS(ImageBindFlags);
