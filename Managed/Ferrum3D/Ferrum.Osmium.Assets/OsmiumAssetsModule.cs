aab7f535f88938cd82234affa6273113a6a4297d
n-dub <ragucoz@gmail.com>
2022-07-07 22:30:08 +0500

    Create `OsmiumAssetsModule` on C# side

--- Managed/Ferrum3D/Ferrum.Osmium.Assets/OsmiumAssetsModule.cs
+++ Managed/Ferrum3D/Ferrum.Osmium.Assets/OsmiumAssetsModule.cs
@@ -1,26 +1,45 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Ferrum.Core.Assets;
+using Ferrum.Core.Framework;
 using Ferrum.Core.Modules;
 
 namespace Ferrum.Osmium.Assets
 {
-    public class OsmiumAssetsModule : IDisposable
+    public class OsmiumAssetsModule : NativeModuleFramework
     {
-        public OsmiumAssetsModule(IntPtr environment)
+        public override DynamicLibrary Library => Factory.Library;
+        private const string LibraryPath = "OsAssets";
+        
+        private OsmiumAssetsModule(IntPtr handle) : base(LibraryPath, handle)
         {
-            AttachEnvironment(environment);
         }
 
-        public void Dispose()
+        public void Initialize(Desc desc)
         {
-            DetachEnvironment();
-            DynamicLibrary.UnloadModule("OsAssetsBindings.dll");
+            base.Initialize();
+            Debug.Assert(AssetManager.Handle != IntPtr.Zero);
+            InitializeNative(Handle, ref desc);
         }
 
-        [DllImport("OsAssetsBindings", EntryPoint = "AttachEnvironment")]
-        private static extern void AttachEnvironment(IntPtr environment);
+        [DllImport(LibraryPath + "Bindings", EntryPoint = "OsmiumAssetsModule_Initialize")]
+        private static extern void InitializeNative(IntPtr self, ref Desc desc);
 
-        [DllImport("OsAssetsBindings", EntryPoint = "DetachEnvironment")]
-        private static extern void DetachEnvironment();
+        public sealed class Factory : NativeModuleFrameworkFactory<OsmiumAssetsModule>
+        {
+            public Factory() : base(LibraryPath)
+            {
+            }
+
+            protected override FrameworkBase CreateFramework(IntPtr handle)
+            {
+                return new OsmiumAssetsModule(handle);
+            }
+        }
+
+        public readonly struct Desc
+        {
+        }
     }
 }
