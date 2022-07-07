2f318000d432f6d9bf602743ded252bcb08626b4
n-dub <ragucoz@gmail.com>
2022-07-07 22:31:38 +0500

    Fix C# Textures sample to work with frameworks

--- Managed/Ferrum3D/Ferrum.Samples.Textures/Program.cs
+++ Managed/Ferrum3D/Ferrum.Samples.Textures/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using Ferrum.Core.Assets;
-using Ferrum.Core.Console;
 using Ferrum.Core.Containers;
+using Ferrum.Core.EventBus;
+using Ferrum.Core.Framework;
 using Ferrum.Core.Math;
-using Ferrum.Core.Modules;
+using Ferrum.Core.Utils;
 using Ferrum.Osmium.Assets;
 using Ferrum.Osmium.GPU;
 using Ferrum.Osmium.GPU.DeviceObjects;
@@ -30,8 +32,10 @@ namespace Ferrum.Samples.Textures
         }
     }
 
-    internal static class Program
+    internal class ExampleApplication : ApplicationFramework
     {
+        public const string ApplicationName = "Ferrum3D - Textures";
+
         private static readonly Vertex[] vertexData =
         {
             new(-0.5f, -0.5f, 0.0f, 1.0f, 0.0f),
@@ -43,51 +47,62 @@ namespace Ferrum.Samples.Textures
         private static readonly uint[] indexData = { 0, 2, 3, 3, 2, 1 };
 
         private static readonly Vector3F[] vsConstantData = { new(0.3f, -0.4f, 0.0f) };
+        private Instance instance;
+        private Adapter adapter;
+        private Device device;
+        private CommandQueue graphicsQueue;
+        private Window window;
+        private SwapChain swapChain;
+        private Buffer constantBuffer;
+        private Buffer indexBuffer;
+        private Buffer vertexBuffer;
+        private DisposableList<Fence> fences;
+        private ShaderModule pixelShader;
+        private ShaderModule vertexShader;
+        private RenderPass renderPass;
+        private GraphicsPipeline pipeline;
+        private DisposableList<Framebuffer> framebuffers;
+        private DisposableList<CommandBuffer> commandBuffers;
+        private CommandQueue commandQueue;
+        private ImageAsset imageAsset;
+        private Image textureImage;
+        private Sampler textureSampler;
+        private DescriptorHeap descriptorHeap;
+        private DescriptorTable descriptorTable;
 
-        private static Buffer CreateHostVisibleBuffer<T>(BindFlags bindFlags, Device device, T[] data)
-            where T : unmanaged
+        protected override bool CloseEventReceived => window.CloseRequested;
+
+        public override void Initialize(Desc desc)
         {
-            var dataLength = (ulong)(data.Length * Marshal.SizeOf<T>());
-            var stagingBuffer = device.CreateBuffer(bindFlags, dataLength);
-            stagingBuffer.AllocateMemory(MemoryType.HostVisible);
-            stagingBuffer.UpdateData(data);
-            return stagingBuffer;
-        }
+            base.Initialize(desc);
+            var gpuModule = GetDependency<OsmiumGpuModule>();
+            gpuModule.Initialize(new OsmiumGpuModule.Desc(ApplicationName, GraphicsApi.Vulkan));
+            var assetsModule = GetDependency<OsmiumAssetsModule>();
+            assetsModule.Initialize(new OsmiumAssetsModule.Desc());
 
-        private static Buffer CreateHostVisibleBuffer(BindFlags bindFlags, Device device, ImageAsset data)
-        {
-            var stagingBuffer = device.CreateBuffer(bindFlags, data.ByteSize);
-            stagingBuffer.AllocateMemory(MemoryType.HostVisible);
-            stagingBuffer.UpdateData(data.DataHandle);
-            return stagingBuffer;
-        }
+            imageAsset = Asset.Load<ImageAsset>(Guid.Parse("94FC6391-4656-4BE7-844D-8D87680A00F1"));
+            instance = gpuModule.CreateInstance();
+            adapter = instance.Adapters.First();
+            device = adapter.CreateDevice();
 
-        private static void RunExample()
-        {
-            using var imageAsset = Asset.Load<ImageAsset>(Guid.Parse("94FC6391-4656-4BE7-844D-8D87680A00F1"));
-            var instanceDesc = new Instance.Desc("Ferrum3D - Textures");
-            using var instance = new Instance(instanceDesc, GraphicsApi.Vulkan);
-            using var adapter = instance.Adapters.First();
-            using var device = adapter.CreateDevice();
+            graphicsQueue = device.GetCommandQueue(CommandQueueClass.Graphics);
 
-            using var graphicsQueue = device.GetCommandQueue(CommandQueueClass.Graphics);
-
-            var windowDesc = new Window.Desc(800, 600, "Ferrum3D - Textures");
-            using var window = device.CreateWindow(windowDesc);
+            var windowDesc = new Window.Desc(desc.WindowWidth, desc.WindowHeight, ApplicationName);
+            window = device.CreateWindow(windowDesc);
 
             var swapChainDesc = new SwapChain.Desc(window, graphicsQueue);
-            using var swapChain = device.CreateSwapChain(swapChainDesc);
+            swapChain = device.CreateSwapChain(swapChainDesc);
 
-            using var constantBuffer = CreateHostVisibleBuffer(BindFlags.ConstantBuffer, device, vsConstantData);
+            constantBuffer = CreateHostVisibleBuffer(BindFlags.ConstantBuffer, device, vsConstantData);
 
             var vertexSize = (ulong)(vertexData.Length * Marshal.SizeOf<Vertex>());
             var indexSize = (ulong)(indexData.Length * sizeof(uint));
 
-            using var vertexBuffer = device.CreateBuffer(BindFlags.VertexBuffer, vertexSize);
+            vertexBuffer = device.CreateBuffer(BindFlags.VertexBuffer, vertexSize);
             vertexBuffer.AllocateMemory(MemoryType.DeviceLocal);
-            using var indexBuffer = device.CreateBuffer(BindFlags.IndexBuffer, indexSize);
+            indexBuffer = device.CreateBuffer(BindFlags.IndexBuffer, indexSize);
             indexBuffer.AllocateMemory(MemoryType.DeviceLocal);
-            using var textureImage = device.CreateImage(Image.Desc.Img2D(
+            textureImage = device.CreateImage(Image.Desc.Img2D(
                 ImageBindFlags.TransferReadWrite | ImageBindFlags.ShaderRead, imageAsset.Width, imageAsset.Height,
                 Format.R8G8B8A8_SRGB, true));
             textureImage.AllocateMemory(MemoryType.DeviceLocal);
@@ -123,17 +138,16 @@ namespace Ferrum.Samples.Textures
                 transferComplete.WaitOnCpu();
             }
 
-            using var textureSampler = device.CreateSampler(Sampler.Desc.Default);
+            textureSampler = device.CreateSampler(Sampler.Desc.Default);
 
-            var compiler = device.CreateShaderCompiler();
+            using var compiler = device.CreateShaderCompiler();
             var vsArgs = ShaderCompiler.Args.FromFile(ShaderStage.Vertex, "Assets/Shaders/VertexShader.hlsl");
             using var vsBytecode = compiler.CompileShader(vsArgs);
             var psArgs = ShaderCompiler.Args.FromFile(ShaderStage.Pixel, "Assets/Shaders/PixelShader.hlsl");
             using var psBytecode = compiler.CompileShader(psArgs);
 
-            using var pixelShader = device.CreateShaderModule(ShaderStage.Pixel, psBytecode);
-            using var vertexShader = device.CreateShaderModule(ShaderStage.Vertex, vsBytecode);
-            compiler.Dispose();
+            pixelShader = device.CreateShaderModule(ShaderStage.Pixel, psBytecode);
+            vertexShader = device.CreateShaderModule(ShaderStage.Vertex, vsBytecode);
 
             var attachmentDesc = new AttachmentDesc(swapChain.Format, ResourceState.Undefined, ResourceState.Present);
             var subpassDesc = new SubpassDesc()
@@ -142,7 +156,7 @@ namespace Ferrum.Samples.Textures
                 .WithAttachments(attachmentDesc)
                 .WithSubpasses(subpassDesc)
                 .WithSubpassDependencies(SubpassDependency.Default);
-            using var renderPass = device.CreateRenderPass(renderPassDesc);
+            renderPass = device.CreateRenderPass(renderPassDesc);
 
             var scissor = window.CreateScissor();
             var viewport = window.CreateViewport();
@@ -155,11 +169,11 @@ namespace Ferrum.Samples.Textures
                     new DescriptorSize(1, ShaderResourceType.ConstantBuffer)
                 );
 
-            using var descriptorHeap = device.CreateDescriptorHeap(descriptorHeapDesc);
+            descriptorHeap = device.CreateDescriptorHeap(descriptorHeapDesc);
             var descriptorSamplerDesc = new DescriptorDesc(ShaderResourceType.Sampler, ShaderStageFlags.Pixel, 1);
             var descriptorImageDesc = new DescriptorDesc(ShaderResourceType.TextureSrv, ShaderStageFlags.Pixel, 1);
             var vsDescriptorDesc = new DescriptorDesc(ShaderResourceType.ConstantBuffer, ShaderStageFlags.Vertex, 1);
-            using var descriptorTable =
+            descriptorTable =
                 descriptorHeap.AllocateDescriptorTable(descriptorSamplerDesc, descriptorImageDesc, vsDescriptorDesc);
 
             descriptorTable.Update(0, textureSampler);
@@ -182,24 +196,24 @@ namespace Ferrum.Samples.Textures
             pipelineDesc.Scissor = scissor;
             pipelineDesc.Viewport = viewport;
 
-            using var pipeline = device.CreateGraphicsPipeline(pipelineDesc);
+            pipeline = device.CreateGraphicsPipeline(pipelineDesc);
 
-            using var fences = new DisposableList<Fence>();
+            fences = new DisposableList<Fence>();
             for (var i = 0; i < swapChain.FrameCount; i++)
             {
                 fences.Add(device.CreateFence(Fence.FenceState.Signaled));
             }
 
-            using var framebuffers = new DisposableList<Framebuffer>();
-            using var commandBuffers = new DisposableList<CommandBuffer>();
+            framebuffers = new DisposableList<Framebuffer>();
+            commandBuffers = new DisposableList<CommandBuffer>();
             for (var i = 0; i < swapChain.ImageCount; i++)
             {
-                var desc = new Framebuffer.Desc()
+                var framebufferDesc = new Framebuffer.Desc()
                     .WithRenderPass(renderPass)
                     .WithScissor(scissor)
                     .WithRenderTargetViews(null, swapChain.RenderTargetViews[i]);
 
-                framebuffers.Add(device.CreateFramebuffer(desc));
+                framebuffers.Add(device.CreateFramebuffer(framebufferDesc));
                 commandBuffers.Add(device.CreateCommandBuffer(CommandQueueClass.Graphics));
 
                 using var builder = commandBuffers[i].Begin();
@@ -210,35 +224,68 @@ namespace Ferrum.Samples.Textures
                 builder.BindVertexBuffer(0, vertexBuffer);
                 builder.BindIndexBuffer(indexBuffer);
                 builder.BeginRenderPass(renderPass, framebuffers[i], Colors.MediumAquamarine);
-                builder.DrawIndexed(6, 1, 0, 0, 0);
+                builder.DrawIndexed(indexData.Length, 1, 0, 0, 0);
                 builder.EndRenderPass();
             }
 
-            using var commandQueue = device.GetCommandQueue(CommandQueueClass.Graphics);
-            while (!window.CloseRequested)
-            {
-                var frameIndex = swapChain.CurrentFrameIndex;
-                fences[frameIndex].WaitOnCpu();
-                window.PollEvents();
-                var imageIndex = swapChain.CurrentImageIndex;
-                fences[swapChain.CurrentFrameIndex].Reset();
-                commandQueue.SubmitBuffers(commandBuffers[imageIndex], fences[frameIndex],
-                    CommandQueue.SubmitFlags.FrameBeginEnd);
-                swapChain.Present();
-            }
-
-            device.WaitIdle();
+            commandQueue = device.GetCommandQueue(CommandQueueClass.Graphics);
         }
 
-        private static void Main()
+        private static Buffer CreateHostVisibleBuffer<T>(BindFlags bindFlags, Device device, T[] data)
+            where T : unmanaged
         {
-            using var engine = new Engine();
-            using var logger = new ConsoleLogger();
-            using var assetManager = new AssetManager("Assets/FerrumAssetIndex");
-            using var osmiumGpuModule = new OsmiumGpuModule(Engine.Environment);
-            using var osmiumAssetsModule = new OsmiumAssetsModule(Engine.Environment);
+            var dataLength = (ulong)(data.Length * Marshal.SizeOf<T>());
+            var stagingBuffer = device.CreateBuffer(bindFlags, dataLength);
+            stagingBuffer.AllocateMemory(MemoryType.HostVisible);
+            stagingBuffer.UpdateData(data);
+            return stagingBuffer;
+        }
 
-            RunExample();
+        private static Buffer CreateHostVisibleBuffer(BindFlags bindFlags, Device device, ImageAsset data)
+        {
+            var stagingBuffer = device.CreateBuffer(bindFlags, data.ByteSize);
+            stagingBuffer.AllocateMemory(MemoryType.HostVisible);
+            stagingBuffer.UpdateData(data.DataHandle);
+            return stagingBuffer;
+        }
+
+        protected override void GetFrameworkDependencies(ICollection<IFrameworkFactory> dependencies)
+        {
+            dependencies.Add(new OsmiumGpuModule.Factory());
+            dependencies.Add(new OsmiumAssetsModule.Factory());
+        }
+
+        protected override void PollSystemEvents()
+        {
+            window.PollEvents();
+        }
+
+        protected override void Tick(FrameEventArgs frameEventArgs)
+        {
+            var frameIndex = swapChain.CurrentFrameIndex;
+            fences[frameIndex].WaitOnCpu();
+            window.PollEvents();
+            var imageIndex = swapChain.CurrentImageIndex;
+            fences[swapChain.CurrentFrameIndex].Reset();
+            commandQueue.SubmitBuffers(commandBuffers[imageIndex], fences[frameIndex],
+                CommandQueue.SubmitFlags.FrameBeginEnd);
+            swapChain.Present();
+        }
+
+        protected override void OnExit()
+        {
+            device.WaitIdle();
+            this.DisposeFields();
+        }
+    }
+
+    internal static class Program
+    {
+        private static int Main()
+        {
+            using var app = new ExampleApplication();
+            app.Initialize(new ApplicationFramework.Desc(ExampleApplication.ApplicationName, "Assets"));
+            return app.RunMainLoop();
         }
     }
 }
