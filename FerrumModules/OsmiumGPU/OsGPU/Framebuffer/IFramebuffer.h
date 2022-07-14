a99ac745ebf5fc40cf1472382ebe061e263e5d2e
n-dub <ragucoz@gmail.com>
2022-07-14 15:53:32 +0500

    Remove unused `Framebuffer.DepthStencilView`

--- FerrumModules/OsmiumGPU/OsGPU/Framebuffer/IFramebuffer.h
+++ FerrumModules/OsmiumGPU/OsGPU/Framebuffer/IFramebuffer.h
@@ -12,7 +12,6 @@ namespace FE::Osmium
         FE_STRUCT_RTTI(FramebufferDesc, "0A969932-5EFF-401E-9318-1C77D190E8A9");
 
         ArraySlice<IImageView*> RenderTargetViews{};
-        Shared<IImageView> DepthStencilView{};
         Shared<IRenderPass> RenderPass{};
         UInt32 Width  = 0;
         UInt32 Height = 0;
