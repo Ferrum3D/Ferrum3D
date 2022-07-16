ff6f9c69d4cde926619c5ccad67dbf29d71ae9f3
n-dub <50988318+n-dub@users.noreply.github.com>
2022-07-16 20:00:43 +0500

    Sort types, calculate capacity and hash in `EntityArchetype`

--- FerrumCore/FeCore/ECS/EntityArchetype.cpp
+++ FerrumCore/FeCore/ECS/EntityArchetype.cpp
@@ -5,5 +5,17 @@ namespace FE::ECS
     EntityArchetype::EntityArchetype(const ArraySlice<ComponentType>& layout)
     {
         m_Layout.Assign(layout.Data(), layout.Data() + layout.Length());
+        InitInternal();
+    }
+
+    void EntityArchetype::InitInternal()
+    {
+        m_Layout.SortByMember(&ComponentType::Alignment, true);
+
+        for (auto& componentType : m_Layout)
+        {
+            m_EntitySize += componentType.AlignedSize();
+            HashCombine(m_HashCode, componentType);
+        }
     }
 } // namespace FE::ECS
