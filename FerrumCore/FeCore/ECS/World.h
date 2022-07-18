911874546c299b65a5ed54be75382ab27f6cc93b
n-dub <ragucoz@gmail.com>
2022-07-18 15:42:09 +0500

    Add `EntityRegistry` and `World`

---
+++ FerrumCore/FeCore/ECS/World.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <FeCore/ECS/EntityRegistry.h>
+
+namespace FE::ECS
+{
+    class World final : Object<IObject>
+    {
+    public:
+        FE_CLASS_RTTI(World, "1937CC6F-4309-4EF4-9AB2-014ABC269FBE");
+    };
+} // namespace FE::ECS
