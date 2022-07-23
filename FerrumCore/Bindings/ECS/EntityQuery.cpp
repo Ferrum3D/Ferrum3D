2ddb070fdbcbd0e3119096e82ebea2e1b50634ae
n-dub <50988318+n-dub@users.noreply.github.com>
2022-07-23 10:33:16 +0500

    Add `EntityQuery` C# bindings

---
+++ FerrumCore/Bindings/ECS/EntityQuery.cpp
@@ -0,0 +1,54 @@
+#include <Bindings/ECS/ComponentType.h>
+#include <FeCore/ECS/EntityQuery.h>
+
+namespace FE::ECS
+{
+    extern "C"
+    {
+        FE_DLL_EXPORT EntityQuery* EntityQuery_Construct(EntityRegistry* registry)
+        {
+            return MakeShared<EntityQuery>(registry).Detach();
+        }
+
+        FE_DLL_EXPORT void EntityQuery_NoneOf(EntityQuery* self, ComponentTypeBinding* componentTypes, UInt32 componentTypeCount)
+        {
+            List<ComponentType> types;
+            types.Reserve(componentTypeCount);
+            for (USize i = 0; i < componentTypeCount; ++i)
+            {
+                types.Push(componentTypes[i].Convert());
+            }
+
+            self->NoneOf(types);
+        }
+
+        FE_DLL_EXPORT void EntityQuery_AllOf(EntityQuery* self, ComponentTypeBinding* componentTypes, UInt32 componentTypeCount)
+        {
+            List<ComponentType> types;
+            types.Reserve(componentTypeCount);
+            for (USize i = 0; i < componentTypeCount; ++i)
+            {
+                types.Push(componentTypes[i].Convert());
+            }
+
+            self->AllOf(types);
+        }
+
+        FE_DLL_EXPORT void EntityQuery_AnyOf(EntityQuery* self, ComponentTypeBinding* componentTypes, UInt32 componentTypeCount)
+        {
+            List<ComponentType> types;
+            types.Reserve(componentTypeCount);
+            for (USize i = 0; i < componentTypeCount; ++i)
+            {
+                types.Push(componentTypes[i].Convert());
+            }
+
+            self->AnyOf(types);
+        }
+
+        FE_DLL_EXPORT void EntityQuery_Destruct(EntityQuery* self)
+        {
+            self->ReleaseStrongRef();
+        }
+    }
+} // namespace FE::ECS
