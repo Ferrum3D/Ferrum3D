ad5ee9b56152bdcc2a26f8c2ae751c096eed211d
n-dub <ragucoz@gmail.com>
2022-07-23 11:39:41 +0500

    C# entity query

---
+++ Managed/Ferrum3D/Ferrum.Core/Utils/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ferrum.Core.Containers;
+
+namespace Ferrum.Core.Utils
+{
+    public static class EnumerableExtensions
+    {
+        public static NativeArray<T> ToNativeArray<T>(this IEnumerable<T> source)
+            where T : unmanaged
+        {
+            if (source is IReadOnlyList<T> list)
+            {
+                return new NativeArray<T>(list);
+            }
+
+            var array = source.ToArray();
+            return new NativeArray<T>(array);
+        }
+    }
+}
