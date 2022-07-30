1de26feb6f084ade6b2cab1256538ec4f8477a22
n-dub <ragucoz@gmail.com>
2022-07-30 19:59:50 +0500

    Add fatal error macros

--- FerrumCore/FeCore/Console/FeLog.h
+++ FerrumCore/FeCore/Console/FeLog.h
@@ -36,6 +36,23 @@ namespace FE
     }                                                                                                                            \
     while (0)
 
+    //! \brief Log an error and break th attached debugger or exit with error code.
+    //!
+    //! Works just like FE_ASSERT_MSG, but indicates that the user input causes this error and doesn't print debug information
+    //! like line number or source file name.
+#define FE_EXPECT_OR_FATAL_ERROR(_Stmt, ...)                                                                                     \
+    do                                                                                                                           \
+    {                                                                                                                            \
+        if (!(_Stmt))                                                                                                            \
+        {                                                                                                                        \
+            FE_LOG_ERROR("Runtime error: {}", ::FE::Fmt::Format(__VA_ARGS__));                                                   \
+            FE_DEBUGBREAK;                                                                                                       \
+            exit(1);                                                                                                             \
+        }                                                                                                                        \
+    }                                                                                                                            \
+    while (0)
+
     //! \brief Equivalent to FE_ASSERT_MSG(false, ...)
 #define FE_UNREACHABLE(...) FE_ASSERT_MSG(false, __VA_ARGS__)
+#define FE_FATAL_ERROR(...) FE_EXPECT_OR_FATAL_ERROR(false, __VA_ARGS__)
 } // namespace FE
